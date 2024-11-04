de110e2babd62c64ee1d7b5d66405f71b3adf83a
ABDELALI AIT TALB <aittalbabdelalie@gmail.com>
2024-11-04 09:52:50 +0100

    Next : implement of path request to change the task Status

--- backend/Tasks/admin.py
+++ backend/Tasks/admin.py
@@ -4,6 +4,6 @@ from . import models
 # Register your models here.
 @admin.register(models.UserTasks)
 class TasksAdmin(admin.ModelAdmin):
-    list_display = ['taskTitle', 'description', 'taskImage', 'created_at']
-    list_editable = ['description']
+    list_display = ['taskTitle', 'description', 'taskImage', 'created_at', 'TaskStatus']
+    list_editable = ['TaskStatus']
     list_per_page = 100
