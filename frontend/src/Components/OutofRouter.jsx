1cc6b071d1f7200af1da9d677b2577802eef35dd
ABDELALI AIT TALB <aittalbabdelalie@gmail.com>
2024-11-06 13:17:52 +0100

    Feat : Add Animation when User visited an unavailable route

--- frontend/src/Components/OutofRouter.jsx
+++ frontend/src/Components/OutofRouter.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import Lottie from 'lottie-react'
+import outOfRouter from './outOfRouter.json'
+
 
 const  Error = () => {
   return (
     <div className='flex items-center justify-center w-full h-[100svh] bg-bodyBackground bg-no-repeat bg-cover bg-center'>
-        <div className='w-[90%] h-[50%] bg-white md:w-[50%] md:h-[60%] text-center'>
-            Fucking Error animation and say to user Error Type Rout now
-
+        <div className='w-[90%] h-[50%]  md:w-[50%] md:h-[60%] text-center flex flex-col gap-3 items-center justify-center'>
+            <h2 className='text-orange font-black'> This Rout is unavailable</h2>
+            <Lottie animationData={outOfRouter}/>
         </div>
     </div>
   )
